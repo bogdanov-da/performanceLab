e3218af87e47d6e2d64481c9a84c76bb749ec640
Dmitriy Bogdanov <bogdanov-da@bk.ru>
2021-08-09 10:04:11 +0300

    Init commit

---
+++ com/company/task1/Task1.java
@@ -0,0 +1,7 @@
+package com.company.task1;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ com/company/task3/Task3.java
@@ -0,0 +1,9 @@
+package com.company.task3;
+
+import jdk.nashorn.internal.parser.JSONParser;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+    }
+}
